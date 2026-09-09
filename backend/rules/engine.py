e35c954fd204f682757fb12b51ff278a8262fe25
HanishBhargav <137503187+HanishBhargav@users.noreply.github.com>
2026-09-09 23:05:14 +0530

    feat: wire MRP_001 into compliance engine

--- backend/rules/engine.py
+++ backend/rules/engine.py
@@ -4,6 +4,7 @@ from .applicability import ApplicabilityStatus, evaluate_chapter_ii
 from .models import ComplianceResult, ComplianceStatus, M2Input, OverallStatus, RuleResult
 from .rule_registry import RuleDefinition, load_rule_registry
 from .validators.commodity_name import validate_commodity_name
+from .validators.mrp_presence import validate_mrp_presence
 from .validators.origin import validate_origin_declaration
 from .validators.party_declaration import validate_party_declaration
 from .validators.quantity import validate_net_quantity
@@ -14,6 +15,7 @@ Validator = Callable[..., RuleResult]
 _VALIDATORS: dict[str, Validator] = {
     "DECL_001": validate_party_declaration,
     "DECL_003": validate_commodity_name,
+    "MRP_001": validate_mrp_presence,
     "QTY_001": validate_net_quantity,
     "QTY_002": validate_quantity_unit,
 }
